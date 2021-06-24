a0d45afa615c38479902d45b9eba2daa16c8fe63
Azat <farscince@gmail.com>
2021-06-24 23:17:43 +0300

    test is working with function in test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,17 +1,15 @@
-//#include <iostream>
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
+//#include <gmock/gmock.h>
 
 
 extern "C" {
-<<<<<<< HEAD:tests/test.cpp
-#include "../GoogleTest/project/project.h"
-=======
-#include "../../includes/minishell.h"
->>>>>>> b69bc247808f96299bcaf0249f527ac120621c4d:GoogleTest/Tests/test.cpp
+#include "../src/includes/minishell.h"
 }
 
-
+int test_fun(int res)
+{
+    return res;
+}
 
 TEST(test_fun, nameOfTheTest1){
     int res = test_fun(1);
