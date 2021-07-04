11b9ed60f4423a33fd3b3e71919230c1010c31f7
Azat <farscince@gmail.com>
2021-07-04 11:52:59 +0300

    copy_array2x was fixed. Header is added

--- src/includes/minishell.h
+++ src/includes/minishell.h
@@ -4,23 +4,57 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <unistd.h>
+# include <string.h>
+# include <errno.h>
 
-typedef struct	s_parser
-{
-	char		*command;
-	char		**args;
-}				t_parser;
+//#include "/Users/yeschall/.brew/opt/readline/include/readline/readline.h"
+//#include "/Users/yeschall/.brew/opt/readline/include/readline/history.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/termios.h>
 
-typedef struct	s_all
+
+# include "../../libft/libft.h"
+#include <sys/stat.h>
+
+/**
+** command	(char **)	list of command words
+** envs		(char **)	copy of envp (malloced)
+** specs	(char)		special symbols:
+**		'0'				no specs
+**		'|'				pipes
+**		'<'				redir1
+**		'>'				redir2
+**		'3'				redir3 >>
+**		'4'				redir4 <<
+**		'5'				||
+**		'6'				&&
+** vpid		(int)		$$ value
+** vlast	(int)		$? value
+*/
+
+typedef struct s_all
 {
-	t_parser 	*command;
-	char 		**envs;
+	char		**command;
+	char		**envs;
 	char		specs;
+	int			vpid;
+	int			vlast;
 }				t_all;
 
 int				test_fun(int res);
 
-void			parser(char *line);
+void			clear_arr_2x(char **arr);
+void			print_array_2x(char **arr);
+char			**copy_arrays_2x(char **src_arr);
+char			*get_value(char **envs, char *key);
+int				is_my_command(char *cmd_name);
+int				parser(char *line, t_all *all);
 
+void			ft_echo(char *messages);
 
 #endif
+
