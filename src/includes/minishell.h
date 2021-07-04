b88d67e420d3b0452972f793b49098383d3b4235
Dinar <dinar95@mail.ru>
2021-07-04 08:09:14 +0300

    fix pareser

--- src/includes/minishell.h
+++ src/includes/minishell.h
@@ -10,22 +10,43 @@
 # include <string.h>
 # include <errno.h>
 # include "../../libft/libft.h"
+#include <sys/stat.h>
 
-typedef struct	s_all
+/**
+** command	(char **)	list of command words
+** envs		(char **)	copy of envp (malloced)
+** specs	(char)		special symbols:
+**		'0'				no specs
+**		'|'				pipes
+**		'<'				redir1
+**		'>'				redir2
+**		'3'				redir3 >>
+**		'4'				redir4 <<
+**		'5'				||
+**		'6'				&&
+** vpid		(int)		$$ value
+** vlast	(int)		$? value
+*/
+
+typedef struct s_all
 {
 	char		**command;
 	char		**envs;
-	char		*specs;
+	char		specs;
+	int			vpid;
+	int			vlast;
 }				t_all;
 
 int				test_fun(int res);
 
-
+void			clear_arr_2x(char **arr);
+void			print_array_2x(char **arr);
+char			**copy_arrays_2x(char **src_arr);
+char			*get_value(char **envs, char *key);
 int				is_my_command(char *cmd_name);
-void			parser(char *line, t_all *all);
-void			init_all(t_all *all);
-char			**ft_get_one_command(char **line,char ***src);
+int				parser(char *line, t_all *all);
 
+void			ft_echo(char *messages);
 
 #endif
 
