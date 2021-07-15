b289b7f3f79bef32989b4dbdbeb96e4352771884
Azat <farscince@gmail.com>
2021-07-15 16:03:21 +0300

    pwd was added

---
+++ src/builtins/ft_pwd.c
@@ -0,0 +1,27 @@
+#include "../includes/minishell.h"
+
+/**
+**
+** @return NEED to FREE
+*/
+char		*get_pwd(void)
+{
+	char	*buf;
+
+	if (!(buf = (char *) malloc(1000)))
+		return (NULL);
+	return (getcwd(buf, 1000));
+}
+
+
+int			ft_pwd(void)
+{
+	char	*buf;
+
+	if (!(buf = get_pwd()))
+		return (-1);
+	// exit(EXIT_FAILURE);
+	printf("%s\n", buf);
+	free(buf);
+	return 1;
+}
