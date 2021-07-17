2042072b2c4acfc87f52dbb0ffc3cbe8c15185af
Azat <farscince@gmail.com>
2021-07-18 00:27:39 +0300

    malloc error testing

--- malloc_test.c
+++ malloc_test.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
 int main()
 {
 	char *tmp = (char *)malloc(100000000000);
 	if (!tmp)
-		printf("Error\n");
+	{
+		printf("Error: %d\n", errno);
+		printf("Do it: %c\n", *tmp);
+		exit(EXIT_FAILURE);
+	}
+
 	printf("Success\n");
 }
