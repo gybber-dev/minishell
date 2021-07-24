69aa839734d158b9e5b93f1f1bcaa7435ffcd9f6
Azat Faezov <farscince@gmail.com>
2021-07-25 00:17:09 +0300

    -n was fixed

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -18,15 +18,11 @@ static int	check_flag_n(char *command_1)
 int	ft_echo(char **command)
 {
 	int		flag;
-	int		tmp;
 
 	flag = 0;
-	tmp = check_flag_n(*(command + 1));
-	while (tmp == 1)
+	while (check_flag_n(*(command + 1)) == 1)
 	{
-		tmp = check_flag_n(*(command + 1));
-		if (tmp == 1)
-			flag = 1;
+		flag = 1;
 		command++;
 	}
 	while (*(++command))
